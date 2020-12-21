772fbdde82e268f745067b3dc3c44c08197aa708
tomoya0605 <t.tomoya0605@gmail.com>
2020-12-22 07:28:11 +0900

    commit

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -14,9 +14,11 @@
 </head>
 <body>
 <div id="app">
+    <header-component></header-component>
 
+    <router-view></router-view>
 </div>
-<!-- Scripts -->
+
 <script src="{{ mix('/js/app.js') }}" defer></script>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,14 @@
 Route::get('/{any}', function() {
     return view('app');
 })->where('any', '.*');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
