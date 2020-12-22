07d01b6dd01fe64b9850f4ec41f888cb2139a264
tomoya0605 <t.tomoya0605@gmail.com>
2020-12-22 09:44:46 +0900

    Revert "commit"
    This reverts commit 4922659e014638572269cb6a819f1f725422c094.

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,3 @@ Route::get('/home', 'HomeController@index')->name('home');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
